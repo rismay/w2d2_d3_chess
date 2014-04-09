a5ce202032259c41c212213480b2b8cf60a3b640
cristian_monterroza <cmonterroza@wrkstrm.me>
2014-04-09 11:32:02 -0700

    Add Pawn class.

--- piece.rb
+++ piece.rb
@@ -140,18 +140,66 @@ class Knight < SteppingPiece
 end
 
 class Pawn < Piece
-  attr_reader :first_move # come back to this for design
+  attr_reader :first_move
+
+  DELTA_B = [[1, 0], [2, 0]]
+  DELTA_Y = [[1, 1], [1, -1]]
+
+  def initialize(board, pos, color)
+    super(board, pos, color)
+    @first_move = true
+  end
 
   def first_move?
-    @first_move ||= false
+    @first_move
+  end
+
+  def delta
+    limit = self.first_move? ? 2 : 1
+    if (self.color == :black)
+      DELTA_B.take(limit)
+    else
+      DELTA_B.take(limit).map{ |x, y| [x * (-1), y * (-1)] }
+    end
+  end
+
+  def delta_y
+    self.color == :black ? DELTA_Y : DELTA_Y.map do |x, y|
+      [x * (-1), y * (-1)]
+    end
   end
 
   def available_moves
-    # Delta for black or white
+    forward_moves + taking_moves
+  end
 
-    # [].tap do |moves_array|
-    #   [0,1] || [0,-1]
-    # end
+  def forward_moves
+    moves_array = []
+    debugger
+    delta.each do |dx, dy|
+      new_pos = [self.pos.first + dx, self.pos.last + dy]
+      if in_board?(new_pos)
+        piece = board[new_pos]
+        if piece.nil?
+          moves_array << new_pos
+        else
+          return moves_array
+        end
+      end
+    end
+    moves_array
+  end
+
+  def taking_moves
+    [].tap do |moves_array|
+      delta_y.each do |dx, dy|
+        new_pos = [self.pos.first + dx, self.pos.last + dy]
+        if in_board?(new_pos)
+          piece = board[new_pos]
+          moves_array << new_pos if !piece.nil? && enemy?(piece)
+        end
+      end
+    end
   end
 
 end
